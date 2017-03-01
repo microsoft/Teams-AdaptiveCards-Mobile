1f8cd49b67db1e21ac085174664fa3e371fc66d6
Matt Hidinger <matt.hidinger@gmail.com>
2017-03-01 15:53:44 -0800

    moved source into platform folders

---
+++ source/shared/dotnet/AdaptiveCards.Model/Elements/Input.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace Adaptive
+{
+    /// <summary>
+    /// Input is used as part of a Form CardElement to collect information from a user
+    /// </summary>
+    public partial class Input : CardElement
+    {
+        public Input()
+        { }
+
+        /// <summary>
+        /// Id for the value (will be used to identify collected input when SUBMIT is clicked)
+        /// </summary>
+        [JsonRequired]
+        [XmlAttribute]
+        public string Id { get; set; }
+    }
+}
