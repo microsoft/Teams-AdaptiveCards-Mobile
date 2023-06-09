451a9c6b6fa1ae8de98ecc197b163a10fceea706
Jvr for school <109031036+Jvr2022@users.noreply.github.com>
2023-06-09 15:07:49 +0200

    Added support for different operating systems
    Added support for different operating systems: The script now detects the operating system and adjusts the command-line argument processing accordingly.
    
    Updated the Java executable detection: The script now uses java instead of java.exe as the executable name, making it compatible with both Windows and Unix-based systems.
    
    Improved command-line argument handling: The script now correctly handles command-line arguments on both Windows and non-Windows platforms.

--- source/android/gradlew.bat
+++ source/android/gradlew.bat
@@ -1,12 +1,12 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem Gradle startup script for Windows, macOS, and Linux
 @rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with windows NT shell
-if "%OS%"=="Windows_NT" setlocal
+@rem Set local scope for the variables
+setlocal
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
@@ -16,10 +16,10 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
-@rem Find java.exe
+@rem Find java executable
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-set JAVA_EXE=java.exe
+set JAVA_EXE=java
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
 
@@ -33,7 +33,7 @@ goto fail
 
 :findJavaFromJavaHome
 set JAVA_HOME=%JAVA_HOME:"=%
-set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+set JAVA_EXE=%JAVA_HOME%/bin/java
 
 if exist "%JAVA_EXE%" goto init
 
@@ -46,45 +46,38 @@ echo location of your Java installation.
 goto fail
 
 :init
-@rem Get command-line arguments, handling Windowz variants
+@rem Get command-line arguments
 
-if not "%OS%" == "Windows_NT" goto win9xME_args
-if "%@eval[2+2]" == "4" goto 4NT_args
-
-:win9xME_args
-@rem Slurp the command line arguments.
 set CMD_LINE_ARGS=
-set _SKIP=2
+if not "%OS%" == "Windows_NT" goto args_non_windows
 
-:win9xME_args_slurp
-if "x%~1" == "x" goto execute
-
+@rem Windows
 set CMD_LINE_ARGS=%*
 goto execute
 
-:4NT_args
-@rem Get arguments from the 4NT Shell from JP Software
-set CMD_LINE_ARGS=%$
+:args_non_windows
+@rem Non-Windows
+for %%x in (%*) do set CMD_LINE_ARGS=!CMD_LINE_ARGS! %%~x
 
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%APP_HOME%/gradle/wrapper/gradle-wrapper.jar
 
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 
 :end
-@rem End local scope for the variables with windows NT shell
+@rem End local scope for the variables
 if "%ERRORLEVEL%"=="0" goto mainEnd
 
 :fail
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
-if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+if not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
 exit /b 1
 
 :mainEnd
-if "%OS%"=="Windows_NT" endlocal
+endlocal
 
 :omega
