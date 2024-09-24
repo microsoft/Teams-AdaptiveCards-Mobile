97241ada74b86e35b3c6e8e022a9e50104b83252
Abhishek Gupta <guptaabhishe@microsoft.com>
2024-09-24 17:01:16 +0530

    Added some tests

---
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCardsTests/AdaptiveCardCarouselTests.mm
@@ -0,0 +1,49 @@
+//
+//  AdaptiveCardCarouselTests.cpp
+//  AdaptiveCardsTests
+//
+//  Created by Abhishek Gupta on 24/09/24.
+//  Copyright © 2024 Microsoft. All rights reserved.
+//
+
+#import "Enums.h"
+#import "ACOAdaptiveCardParseResult.h"
+#import "ACOAdaptiveCard.h"
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "CompoundButton.h"
+
+@interface AdaptiveCardsCarouselTests : XCTestCase
+
+@end
+
+@implementation AdaptiveCardsCarouselTests{
+    NSBundle *_classBundle;
+}
+
+- (void)setUp
+{
+    _classBundle = [NSBundle bundleForClass:[self class]];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testCarouselValidParsedResult
+{
+    NSString *payload = [NSString stringWithContentsOfFile:[_classBundle pathForResource:@"Carousel.valid" ofType:@"json"] encoding:NSUTF8StringEncoding error:nil];
+    ACOAdaptiveCardParseResult *cardParseResult = [ACOAdaptiveCard fromJson:payload];
+    XCTAssertTrue(cardParseResult.isValid);
+}
+
+- (void)testCarouselInvalidParsedResult
+{
+    NSString *payload = [NSString stringWithContentsOfFile:[_classBundle pathForResource:@"Carousel.invalid" ofType:@"json"] encoding:NSUTF8StringEncoding error:nil];
+    ACOAdaptiveCardParseResult *cardParseResult = [ACOAdaptiveCard fromJson:payload];
+    XCTAssertFalse(cardParseResult.isValid);
+}
+
+@end
+
