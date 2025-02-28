410edf5e3ecebb7cebf6deacdab9e1bd68b75840
Harika P <harikap@Harikas-MacBook-Pro.local>
2025-02-28 20:55:54 +0530

    Minor change

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRButton.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRButton.mm
@@ -284,7 +284,7 @@
         UIImageView *iconView = [[ACRUIImageView alloc] init];
         UIImage *image = [UIImage systemImageNamed:@"chevron.down"];
         iconView.image = image;
-        iconView.tintColor = button.titleLabel.textColor;
+        iconView.tintColor = [config getTextBlockColor:(ACRContainerStyle::ACRAccent) textColor:(ForegroundColor::Accent)subtleOption:false];
         [button addSubview:iconView];
         button.iconView = iconView;
         [button setImageView:image withConfig:config];
