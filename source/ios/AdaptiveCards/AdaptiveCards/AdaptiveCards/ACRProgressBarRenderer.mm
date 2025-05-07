7ebdb9a122e48efa4e773ffc6205c3f89d4afbef
Harika <harikap@Harikas-MacBook-Pro.local>
2025-05-07 17:21:24 +0530

    Created files for Progress bar and ring

---
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRProgressBarRenderer.mm
@@ -0,0 +1,37 @@
+//
+//  ACRProgressBarRenderer.mm
+//  AdaptiveCards
+//
+//  Created by Harika P on 07/05/25.
+//  Copyright © 2025 Microsoft. All rights reserved.
+//
+
+#import "ACRProgressBarRenderer.h"
+#import "ACOBaseCardElementPrivate.h"
+#import "ACOHostConfigPrivate.h"
+#import "ACRRegistration.h"
+#import "UtiliOS.h"
+
+@implementation ACRProgressBarRenderer
+
++ (ACRProgressBarRenderer *)getInstance
+{
+    static ACRProgressBarRenderer *singletonInstance = [[self alloc] init];
+    return singletonInstance;
+}
+
++ (ACRCardElementType)elemType
+{
+    return ACRProgressBar;
+}
+
+- (UIView *)render:(UIView<ACRIContentHoldingView> *)viewGroup
+           rootView:(ACRView *)rootView
+             inputs:(NSMutableArray *)inputs
+    baseCardElement:(ACOBaseCardElement *)acoElem
+         hostConfig:(ACOHostConfig *)acoConfig
+{
+    return [[UIView alloc] init];
+}
+
+@end
