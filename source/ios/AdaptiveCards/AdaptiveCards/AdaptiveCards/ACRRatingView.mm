244cd9f19ba65444e04acae3d76af6d2a3d9bfb6
Harika <harikap@Harikas-MacBook-Pro.local>
2025-06-18 21:13:48 +0530

    Figma changes

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRRatingView.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRRatingView.mm
@@ -35,7 +35,7 @@
 
 {
     self = [super initWithFrame:CGRectZero];
-    if (self) 
+    if (self)
     {
         _max = max;
         _value = value;
@@ -60,7 +60,7 @@
                            hostConfig:(ACOHostConfig *)hostConfig
 {
     self = [super initWithFrame:CGRectZero];
-    if (self) 
+    if (self)
     {
         _max = max;
         _value = value;
@@ -141,9 +141,9 @@
     [self updateStarImages];
 }
 
-- (void)setupConstraints 
+- (void)setupConstraints
 {
-    CGFloat gap = _readOnly ? 4 : 12;
+    CGFloat gap = _readOnly ? 2 : 8;
     for (NSUInteger i = 0; i < _starImageViews.count; i++)
     {
         UIImageView *starImageView = _starImageViews[i];
@@ -169,14 +169,14 @@
                 [starImageView.leadingAnchor constraintEqualToAnchor:previousStarImageView.trailingAnchor constant:gap]
             ]];
         }
-        
+        CGFloat padding = _readOnly ? 0 : (_ratingSize == ACRMedium) ? 8 : 6;
         // Width and height constraints for each star
         [NSLayoutConstraint activateConstraints:@[
-            [starImageView.widthAnchor constraintEqualToConstant:[self sizeOfStar].width],
-            [starImageView.heightAnchor constraintEqualToConstant:[self sizeOfStar].height]
+            [starImageView.widthAnchor constraintEqualToConstant:[self sizeOfStar].width + (padding * 2)],
+            [starImageView.heightAnchor constraintEqualToConstant:[self sizeOfStar].height + (padding *2)]
         ]];
         
-        if (i == _starImageViews.count - 1) 
+        if (i == _starImageViews.count - 1)
         {
             // Trailing constraint for the last star
             if(_ratingLabel != nil)
@@ -227,7 +227,7 @@
     return filledStarImage;
 }
 
-- (void)handleStarTap:(UITapGestureRecognizer *)gesture 
+- (void)handleStarTap:(UITapGestureRecognizer *)gesture
 {
     UIImageView *tappedStar = (UIImageView *)gesture.view;
     NSInteger index = [_starImageViews indexOfObject:tappedStar];
@@ -236,13 +236,13 @@
     [_ratingValueChangeDelegate didChangeValueTo:_value];
 }
 
-- (void)updateStarImages 
+- (void)updateStarImages
 {
     NSUInteger totalFilledStars = (NSUInteger)_value;
-    for (NSUInteger i = 0; i < _starImageViews.count; i++) 
+    for (NSUInteger i = 0; i < _starImageViews.count; i++)
     {
         UIImageView *starImageView = _starImageViews[i];
-        if (i < totalFilledStars) 
+        if (i < totalFilledStars)
         {
             starImageView.image = [self filledStarImage];
             starImageView.tintColor = [self colorForFilledStars];
@@ -291,16 +291,14 @@
 
 - (CGSize)sizeOfStar
 {
-    switch (_ratingSize) 
+    switch (_ratingSize)
     {
         case ACRMedium:
-            return _readOnly ? CGSizeMake(24, 24) : CGSizeMake(28, 28);
+            return _readOnly ? CGSizeMake(16, 16) : CGSizeMake(28, 28);
             
         case ACRLarge:
-            return _readOnly ? CGSizeMake(28, 28): CGSizeMake(32, 32);
-            
         default:
-            return CGSizeMake(28, 28);
+            return _readOnly ? CGSizeMake(20, 20): CGSizeMake(32, 32);
     }
 }
 
