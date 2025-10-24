cfaa4a5d6b75f213013487078e7e45d49871b03a
Gaurav Keshre <gakeshre@microsoft.com>
2025-10-24 19:58:02 +0530

    added custom classes necessary to render uiview subclasses inside the uitextview with nsattributedstring

---
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRViewAttachingTextView.mm
@@ -0,0 +1,58 @@
+//
+//  ACRViewAttachingTextView.mm
+//  AdaptiveCards
+//
+//  Created by Gaurav Keshre on 24/10/25.
+//  Copyright © 2025 Microsoft. All rights reserved.
+//
+
+#import "ACRViewAttachingTextView.h"
+#import "ACRViewAttachingTextViewBehavior.h"
+
+@interface ACRViewAttachingTextView ()
+@property (nonatomic, strong) ACRViewAttachingTextViewBehavior *attachmentBehavior;
+@end
+
+@implementation ACRViewAttachingTextView
+
+- (instancetype)initWithFrame:(CGRect)frame textContainer:(NSTextContainer *)textContainer {
+    self = [super initWithFrame:frame textContainer:textContainer];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder {
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self commonInit];
+    }
+    return self;
+}
+
+- (void)commonInit {
+    // Connect the attachment behavior
+    self.attachmentBehavior = [[ACRViewAttachingTextViewBehavior alloc] init];
+    self.attachmentBehavior.textView = self;
+    self.layoutManager.delegate = self.attachmentBehavior;
+    self.textStorage.delegate = self.attachmentBehavior;
+}
+
+- (void)setTextContainerInset:(UIEdgeInsets)textContainerInset {
+    [super setTextContainerInset:textContainerInset];
+    
+    // Text container insets are used to convert coordinates between the text container and text view,
+    // so a change to these insets must trigger a layout update
+    [self.attachmentBehavior layoutAttachedSubviews];
+}
+
+@end
