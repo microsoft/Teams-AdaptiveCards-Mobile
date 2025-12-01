0faf6c20b614354d9a9993fa55f1cd69d32e997a
Harika <harikap@Harikas-MacBook-Pro.local>
2025-12-01 18:36:42 +0530

    Fixed popover visibility when keyboard is popped

--- source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRBottomSheetPresentationController.mm
+++ source/ios/AdaptiveCards/AdaptiveCards/AdaptiveCards/ACRBottomSheetPresentationController.mm
@@ -11,24 +11,42 @@
 @interface ACRBottomSheetPresentationController ()
 
 @property (nonatomic, strong) UIView *dimmingView;
+@property (nonatomic) CGFloat keyboardHeight;
+@property (nonatomic) BOOL isAdjustingForKeyboard;
 
 @end
 
 @implementation ACRBottomSheetPresentationController
 
+#pragma mark - Frame Calculation
+
 - (CGRect) frameOfPresentedViewInContainerView
 {
     CGFloat targetHeight = self.presentedViewController.preferredContentSize.height;
-    return CGRectMake(0, self.containerView.bounds.size.height - targetHeight, self.containerView.bounds.size.width, targetHeight);
+    CGFloat containerHeight = self.containerView.bounds.size.height;
+    CGFloat visibleHeight = containerHeight - self.keyboardHeight;
+    CGFloat y = visibleHeight - targetHeight;
+    y = y < 0 ? 0 : y;
+
+    return CGRectMake(0, y, self.containerView.bounds.size.width, targetHeight);
 }
+
+#pragma mark - Layout
 
 - (void) containerViewWillLayoutSubviews
 {
     self.dimmingView.frame = self.containerView.bounds;
-    self.presentedView.frame = [self frameOfPresentedViewInContainerView];
+
+    if (!self.isAdjustingForKeyboard)
+    {
+        self.presentedView.frame = [self frameOfPresentedViewInContainerView];
+    }
+
     self.presentedView.layer.cornerRadius = 10;
     self.presentedView.clipsToBounds = YES;
 }
+
+#pragma mark - Presentation
 
 - (void) presentationTransitionWillBegin
 {
@@ -47,7 +65,20 @@
     [UIView animateWithDuration:0.25 animations:^{
         weakSelf.dimmingView.alpha = 1.0;
     }];
+
+    // Register keyboard observers
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillShow:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
 }
+
+#pragma mark - Dismissal
 
 - (void) dismissalTransitionWillBegin
 {
@@ -63,8 +94,43 @@
     {
         [self.dimmingView removeFromSuperview];
         self.dimmingView = nil;
+
+        [[NSNotificationCenter defaultCenter] removeObserver:self];
     }
 }
+
+#pragma mark - Keyboard Handling
+
+- (void) keyboardWillShow:(NSNotification *)notification
+{
+    CGRect kbFrame = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+    self.keyboardHeight = kbFrame.size.height;
+    self.isAdjustingForKeyboard = YES;
+
+    CGRect newFrame = [self frameOfPresentedViewInContainerView];
+
+    [UIView animateWithDuration:0.25 animations:^{
+        self.presentedView.frame = newFrame;
+    } completion:^(BOOL finished) {
+        self.isAdjustingForKeyboard = NO;
+    }];
+}
+
+- (void) keyboardWillHide:(NSNotification *)notification
+{
+    self.keyboardHeight = 0;
+    self.isAdjustingForKeyboard = YES;
+
+    CGRect newFrame = [self frameOfPresentedViewInContainerView];
+
+    [UIView animateWithDuration:0.25 animations:^{
+        self.presentedView.frame = newFrame;
+    } completion:^(BOOL finished) {
+        self.isAdjustingForKeyboard = NO;
+    }];
+}
+
+#pragma mark - Dimming Tap
 
 - (void) handleDimmingViewTap:(UITapGestureRecognizer *)gesture
 {
