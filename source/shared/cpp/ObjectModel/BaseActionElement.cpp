d24e1f5af843adeaf4b41d1dc7e0d919e800b499
meethjain <85880402+meethjain@users.noreply.github.com>
2026-02-05 22:12:14 +0530

    AC SDK Changes for Web Rendering (#489)
    * Sample
    
    * Samples
    
    * Samples
    
    * Samples
    
    * Added tab set testing json
    
    * Inline icons and images json
    
    * Badge json
    
    * Update Input_Rating.json
    
    * Sample
    
    * Sample
    
    * Sample
    
    * Sample
    
    * SVG Path changes
    
    * SVG Path changes
    
    * Minor changes
    
    * Updated PR
    
    ---------
    
    Co-authored-by: Harika <harikap@Harikas-MacBook-Pro.local>
    Co-authored-by: Harika <51773380+P-Harika@users.noreply.github.com>

--- source/shared/cpp/ObjectModel/BaseActionElement.cpp
+++ source/shared/cpp/ObjectModel/BaseActionElement.cpp
@@ -49,19 +49,7 @@ std::string BaseActionElement::GetSVGPath() const
 
 std::string BaseActionElement::GetSVGPath(const std::string& iconUrl) const
 {
-    std::regex regex{R"([,:]+)"}; // split on ':' and ','
-    std::sregex_token_iterator it{iconUrl.begin(), iconUrl.end(), regex, -1};
-    std::vector<std::string> config{it, {}};
-    std::string iconStyle = IconStyleToString(IconStyle::Regular);
-    std::string iconName = "";
-    if(config.size() >= 2) {
-        iconName = config[1];
-        if(config.size() > 2) {
-            iconStyle = config[config.size() - 1];
-        }
-    }
-    std::string m_url = iconName + "/" + iconName + ".json";
-    return m_url;
+    return BaseActionElement::GetSVGPathForIconUrl(iconUrl);
 }
 
 void BaseActionElement::SetIconUrl(std::string&& value)
